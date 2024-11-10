e99d57f7e20a65079047c5ada316f292f35e42b8
GGTuran <tayyabturan67@gmail.com>
2024-11-10 19:31:27 +0600

    added schemas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser'
 
 const app: Application = express();
 
 app.use(cors());
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
